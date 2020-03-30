c6459a38d2988b484b6525dbd3a798fb8cda2834
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-30 18:40:21 +0430

    add cmd

--- lib/cmd.php
+++ lib/cmd.php
@@ -16,6 +16,7 @@ class jibresAppCmd
 		$cmd_runGradle = 'cd '.APP_FOLDER. ' && ./gradlew clean';
 		$output = shell_exec($cmd_runGradle);
 		jibresAppCode::log($output, true);
+		self::$checkSuccess($output);
 	}
 
 
@@ -24,6 +25,7 @@ class jibresAppCmd
 		$cmd_runGradle = 'cd '.APP_FOLDER. ' && ./gradlew build';
 		$output = shell_exec($cmd_runGradle);
 		jibresAppCode::log($output, true);
+		self::$checkSuccess($output);
 	}
 
 
@@ -32,6 +34,37 @@ class jibresAppCmd
 		$cmd_runGradle = 'cd '.APP_FOLDER. ' && ./gradlew assembleRelease';
 		$output = shell_exec($cmd_runGradle);
 		jibresAppCode::log($output, true);
+		self::$checkSuccess($output);
+	}
+
+	private static function checkSuccess($_response)
+	{
+		if(strpos($_response, 'BUILD SUCCESSFUL in ') !== false)
+		{
+			$myResponse = str_replace(["\r\n","\n\r","\r"],"\n", $_response);
+			$myResponseArr = explode("\n", $myResponse);
+			$runSecond = null;
+			foreach ($myResponseArr as $key => $myLine)
+			{
+				if($myStartPos = strpos($myLine, 'BUILD SUCCESSFUL in ') !== false)
+				{
+					// this is result line
+					$runSecond = substr($myLine, $myStartPos);
+				}
+			}
+
+			var_dump($runSecond);
+			var_dump($myResultArr);
+
+			// $startPos = strpos($_response, 'BUILD SUCCESSFUL in ');
+			// return true;
+		}
+		var_dump($myResponse);
+
+		exit();
+
+		// build failed
+		return false;
 	}
 }
 ?>
