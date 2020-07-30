e413cec8333a6ea37a4e396ff9a94e47e292ad55
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-30 13:07:59 +0430

    bugfix create empty folder

--- lib/file.php
+++ lib/file.php
@@ -6,15 +6,16 @@ class jibresAppFile
 	{
 		$factoryAPK = APP_FOLDER. '/app/build/outputs/apk/release/app-release.apk';
 
-		$myDestDir = dirname($_to);
-		if (!is_dir($myDestDir))
-		{
-			// dir doesn't exist, make it
-			mkdir($myDestDir, 0775, true);
-		}
 
 		if(file_exists($factoryAPK))
 		{
+			$myDestDir = dirname($_to);
+			if (!is_dir($myDestDir))
+			{
+				// dir doesn't exist, make it
+				mkdir($myDestDir, 0775, true);
+			}
+			// copy file
 			copy($factoryAPK, $_to);
 		}
 		else
